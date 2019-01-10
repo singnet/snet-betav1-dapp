9bc277f595a74deee30c513e6861ceb9864f1634
raamb <raam.bd@gmail.com>
2019-01-10 17:56:44 +0530

    Pulled out header into its own component

--- src/components/Services.js
+++ src/components/Services.js
@@ -700,28 +700,10 @@ class SampleServices extends React.Component {
       </div>
     );
     
-    //<App searchTerm={this.state.searchterm} searchCallBack={this.onOpenSearchBar}/>
     let CallComponent = this.serviceMappings.getComponent(this.serviceState.orgId, this.serviceState.serviceId);
     return(
           <React.Fragment>            
-            <div className="inner">
-                <div className="header">
-                    <div className="col-xs-6 col-sm-4 col-md-6 col-lg-6 logo">
-                        <h1><img src="./img/singularity-logo.png"  alt="SingularityNET"/></h1>
-                    </div>
-                    <div className="col-xs-6 col-sm-8 col-md-6 col-lg-6 search-user">
-                        <input className="search hidden-xs" placeholder={this.state.searchterm} name="srch-term" id="srch-term" type="label" onClick={this.onOpenSearchBar} />
-                        <div className="user">
-                            {(typeof web3 !== 'undefined')?
-                            <Link to="/SampleServices"><img src="./img/home-icon.png" alt="" /> </Link>:
-                            <Link to="/"><img src="./img/home-icon.png" alt="" /> </Link>}
-                        </div>
-                        <div className="user">
-                            <Link to="/Profile"><img src="./img/user.png" alt="User" /></Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <App searchTerm={this.state.searchterm} searchCallBack={this.onOpenSearchBar}/>
             <main role="content" className="content-area">
                 <div className="container-fluid p-4  ">                                     
                     <Carddeckers/>                    
