c0f975158a5e90d6b6d4110dbc5c888012c714ca
anand <anand@singularitynet.io>
2019-02-10 19:11:05 +0530

    Added method componentWillReceiveProps and removed the BaseService

--- src/components/service/ExampleService.js
+++ src/components/service/ExampleService.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {hasOwnDefinedProperty} from '../../util'
-import BaseService from './BaseService';
 
-export default class ExampleService extends BaseService {
+export default class ExampleService extends React.Component {
 
     constructor(props) {
         super(props);
@@ -17,8 +16,63 @@ export default class ExampleService extends BaseService {
             a: 0,
             b: 0
         };
+
+        this.isComplete = false;
+        this.serviceMethods = [];
+        this.allServices = [];
+        this.methodsForAllServices = [];
+        this.parseProps(props);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if(this.isComplete !== nextProps.isComplete) {
+            this.parseProps(nextProps);
+        }
+    }
+
+    parseProps(nextProps) {
+        this.isComplete = nextProps.isComplete;
+        if (!this.isComplete) {
+            this.parseServiceSpec(nextProps.serviceSpec);
+        } else {
+            if (typeof nextProps.response !== 'undefined') {
+                if (typeof nextProps.response === 'string') {
+                    this.state.response = nextProps.response;
+                } else {
+                    this.state.response = nextProps.response.value;
+                }
+            }
+        }
+    }
+
+    parseServiceSpec(serviceSpec) {
+        const packageName = Object.keys(serviceSpec.nested).find(key =>
+            typeof serviceSpec.nested[key] === "object" &&
+            hasOwnDefinedProperty(serviceSpec.nested[key], "nested"));
+
+        var objects = undefined;
+        var items = undefined;
+        if (typeof packageName !== 'undefined') {
+            items = serviceSpec.lookup(packageName);
+            objects = Object.keys(items);
+        } else {
+            items = serviceSpec.nested;
+            objects = Object.keys(serviceSpec.nested);
+        }
+
+        this.allServices.push("Select a service");
+        this.methodsForAllServices = [];
+        objects.map(rr => {
+            if (typeof items[rr] === 'object' && items[rr] !== null && items[rr].hasOwnProperty("methods")) {
+                this.allServices.push(rr);
+                this.methodsForAllServices.push(rr);
+
+                var methods = Object.keys(items[rr]["methods"]);
+                methods.unshift("Select a method");
+                this.methodsForAllServices[rr] = methods;
+            }
+        })
+    }
 
     handleFormUpdate(event) {
         this.setState({
@@ -31,7 +85,29 @@ export default class ExampleService extends BaseService {
         this.setState({
             serviceName: strService
         });
-        super.handleServiceName(strService);
+        this.serviceMethods.length = 0;
+        if (typeof strService !== 'undefined' && strService !== 'Select a service') {
+            let data = Object.values(this.methodsForAllServices[strService]);
+            if (typeof data !== 'undefined') {
+                this.serviceMethods= data;
+            }
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(this.isComplete !== nextProps.isComplete) {
+            this.parseProps(nextProps);
+        }
+    }
+    onKeyPressvalidator(event) {
+        const keyCode = event.keyCode || event.which;
+        if (!(keyCode == 8 || keyCode == 46) && (keyCode < 48 || keyCode > 57)) {
+            event.preventDefault()
+        } else {
+            let dots = event.target.value.split('.');
+            if (dots.length > 1 && keyCode == 46)
+                event.preventDefault()
+        }
     }
 
     submitAction() {
