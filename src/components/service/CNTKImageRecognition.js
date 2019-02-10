56bb8e0b1981aa5af3e24d468caf9689c48759f6
anand <anand@singularitynet.io>
2019-02-10 13:57:33 +0530

    Added method componentWillReceiveProps

--- src/components/service/CNTKImageRecognition.js
+++ src/components/service/CNTKImageRecognition.js
@@ -2,9 +2,8 @@ import React from 'react';
 import {hasOwnDefinedProperty} from '../../util'
 import Button from '@material-ui/core/Button';
 import SNETImageUpload from "./standardComponents/SNETImageUpload";
-import BaseService from "./BaseService";
 
-export default class CNTKImageRecognition extends BaseService {
+export default class CNTKImageRecognition extends React.Component {
 
     constructor(props) {
         super(props);
@@ -46,6 +45,39 @@ export default class CNTKImageRecognition extends BaseService {
         }
     }
 
+    componentWillReceiveProps(nextProps) {
+        if(this.isComplete !== nextProps.isComplete) {
+            this.parseProps(nextProps);
+        }
+    }
+    parseServiceSpec(serviceSpec) {
+        const packageName = Object.keys(serviceSpec.nested).find(key =>
+            typeof serviceSpec.nested[key] === "object" &&
+            hasOwnDefinedProperty(serviceSpec.nested[key], "nested"));
+
+        var objects = undefined;
+        var items = undefined;
+        if (typeof packageName !== 'undefined') {
+            items = serviceSpec.lookup(packageName);
+            objects = Object.keys(items);
+        } else {
+            items = serviceSpec.nested;
+            objects = Object.keys(serviceSpec.nested);
+        }
+
+        this.methodsForAllServices = [];
+        objects.map(rr => {
+            if (typeof items[rr] === 'object' && items[rr] !== null && items[rr].hasOwnProperty("methods")) {
+                this.allServices.push(rr);
+                this.methodsForAllServices.push(rr);
+
+                var methods = Object.keys(items[rr]["methods"]);
+                methods.unshift("Select a method");
+                this.methodsForAllServices[rr] = methods;
+            }
+        });
+        this.getServiceMethods(this.allServices[0]);
+    }
 
     getServiceMethods(strService) {
         this.setState({
