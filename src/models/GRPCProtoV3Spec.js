71f3bfa300c79e0858689ed32fc2aa08d43feaad
chetankothari <chetankothari@users.noreply.github.com>
2019-02-14 22:45:15 +0530

    Add grpc spec parsing

---
+++ src/models/GRPCProtoV3Spec.js
@@ -0,0 +1,52 @@
+import _ from 'lodash';
+import { hasOwnDefinedProperty } from "../util";
+import GRPCV3Service from "./GRPCV3Service";
+
+export default class GRPCProtoV3Spec {
+  constructor(serviceSpec) {
+    this._serviceSpec = serviceSpec;
+    this._services = this.parseServices();
+  }
+
+  parseServices() {
+    const packageName = this._getPackageName();
+    const specDefinitions = this._getSpecDefinitions(packageName);
+
+    function hasMethods(specDefinition) {
+      return specDefinition.hasOwnProperty("methods");
+    }
+
+    const services = _.reduce(specDefinitions, (serviceAccumulator, specDefinition, specKey) => {
+      const isService = typeof specDefinition === 'object' && specDefinition !== null && hasMethods(specDefinition);
+
+      if(!isService) {
+        return serviceAccumulator;
+      }
+
+      const service = new GRPCV3Service(specKey, specDefinition);
+      return serviceAccumulator.push(service);
+    });
+
+    return services;
+  }
+
+  _getPackageName() {
+    const packageName = _.findKey(this._serviceSpec.nested, (obj) => {
+      return typeof obj === "object" && hasOwnDefinedProperty(obj, "nested");
+    });
+
+    return packageName || '';
+  }
+
+  _getSpecDefinitions(packageName) {
+    if(packageName) {
+      return this._serviceSpec.lookup(packageName);
+    }
+
+    return this._serviceSpec.nested;
+  }
+
+  get services() {
+    return [...this._services];
+  }
+}
