e809c9603cec33d5e43aceb460d2c72a8c560d12
Yisehak <yisehak.aw@gmail.com>
2019-02-18 19:46:05 +0300

    removed parse props implementation from moses service

--- src/components/service/ServiceMappings.js
+++ src/components/service/ServiceMappings.js
@@ -26,69 +26,27 @@ import DefaultService from "./DefaultService.js";
 export default class SampleServices {
   constructor() {
     this.serviceOrgIDToComponent = [];
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "example-service")
-    ] = ExampleService;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "cntk-image-recon")
-    ] = CNTKImageRecognition;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "cntk-next-day-trend")
-    ] = CNTKNextDayTrend;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "cntk-lstm-forecast")
-    ] = CNTKLSTMForecast;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "cntk-language-understanding")
-    ] = CNTKLanguageUnderstanding;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "i3d-video-action-recognition")
-    ] = I3DActionRecognition;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "opennmt-romance-translator")
-    ] = OpenNMTRomanceTranslator;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "s2vt-video-captioning")
-    ] = S2VTVideoCaptioning;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "yolov3-object-detection")
-    ] = YOLOv3ObjectDetection;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "zeta36-chess-alpha-zero")
-    ] = Zeta36ChessAlphaZero;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "speech-recognition")
-    ] = AutomaticSpeechRecognition;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "speech-synthesis")
-    ] = NeuralSpeechSynthesis;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "question-answering-long-seq")
-    ] = LongQuestionAsnswering;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "question-answering-short-seq")
-    ] = ShortQuestionAnswering;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "semantic-similarity-binary")
-    ] = BinarySemanticSimilarity;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "opencog-vqa")
-    ] = VisualQAOpencog;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "named-entity-recognition")
-    ] = NamedEntityRecognitionService;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "sentiment-analysis")
-    ] = SentimentAnalysisService;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "time-series-anomaly-discovery")
-    ] = TimeSeriesAnomalyDiscoveryService;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "moses-service")
-    ] = MosesService;
-    this.serviceOrgIDToComponent[
-      this.generateUniqueID("snet", "annotation-service")
-    ] = GeneAnnotationService;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "example-service")] = ExampleService;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "cntk-image-recon")] = CNTKImageRecognition;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "cntk-next-day-trend")] = CNTKNextDayTrend;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "cntk-lstm-forecast")] = CNTKLSTMForecast;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "cntk-language-understanding")] = CNTKLanguageUnderstanding;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "i3d-video-action-recognition")] = I3DActionRecognition;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "opennmt-romance-translator")] = OpenNMTRomanceTranslator;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "s2vt-video-captioning")] = S2VTVideoCaptioning;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "yolov3-object-detection")] = YOLOv3ObjectDetection;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "zeta36-chess-alpha-zero")] = Zeta36ChessAlphaZero;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "speech-recognition")] = AutomaticSpeechRecognition;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "speech-synthesis")] = NeuralSpeechSynthesis;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "question-answering-long-seq")] = LongQuestionAsnswering;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "question-answering-short-seq")] = ShortQuestionAnswering;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "semantic-similarity-binary")] = BinarySemanticSimilarity;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "opencog-vqa")] = VisualQAOpencog;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "named-entity-recognition")] = NamedEntityRecognitionService;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "sentiment-analysis")] = SentimentAnalysisService;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "time-series-anomaly-discovery")] = TimeSeriesAnomalyDiscoveryService;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "moses-service")] = MosesService;
+    this.serviceOrgIDToComponent[this.generateUniqueID("snet", "annotation-service")] = GeneAnnotationService;
   }
 
   generateUniqueID(orgId, serviceId) {
