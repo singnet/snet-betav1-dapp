b631076c95a604f4e7265cc547c94b0a9f8279a9
chetankothari <chetankothari@users.noreply.github.com>
2019-02-22 15:09:21 +0530

    Upgrade webpack 4.16.2 -> 4.29.5

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,8 +1,6 @@
 var webpack = require('webpack');
 var merge = require('webpack-merge');
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var CompressionPlugin = require('compression-webpack-plugin');
-
 var common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -10,14 +8,6 @@ module.exports = merge(common, {
 
   plugins: [
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-    new UglifyJSPlugin({
-      sourceMap: true,
-      uglifyOptions: {
-        compress: true,
-        mangle: true,
-        warnings: false
-      }
-    }),
     new CompressionPlugin({
       test: /\.js/,
       filename (asset) {
