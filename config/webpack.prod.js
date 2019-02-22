c5724fe71632c171842bcf47e92134627865d3dc
chetankothari <chetankothari@users.noreply.github.com>
2019-02-22 15:11:43 +0530

    Configure terser-webpack-plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,11 +1,20 @@
 var webpack = require('webpack');
 var merge = require('webpack-merge');
 var CompressionPlugin = require('compression-webpack-plugin');
+var TerserPlugin = require('terser-webpack-plugin');
 var common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+    ],
+  },
   plugins: [
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
     new CompressionPlugin({
