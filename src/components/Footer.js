ecc3528276c2e4ea4209e5e9b514af95cab5720f
raamb <raam.bd@gmail.com>
2019-02-28 15:01:33 +0530

    Added TOS Show Faucet for testnets only

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const tosKey="hasACC3p3t3d"
+export default class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      acceptedTOC: this.hasAcceptedTOC()
+    }
+    this.handleAcceptedTOC = this.handleAcceptedTOC.bind(this);
+    console.log("Footer " + this.state.acceptedTOC)
+  }
+
+  hasAcceptedTOC() {
+    console.log("hasAccepted " + window.localStorage.getItem(tosKey));
+    return window.localStorage.getItem(tosKey) || false;
+  }
+  
+  acceptTOC() {
+    window.localStorage.setItem(tosKey, true);
+    // close/hide notification
+  }
+
+  handleAcceptedTOC(){
+    this.setState({
+      acceptedTOC: true
+    })
+    this.acceptTOC()
+  }
+
+
+  render() {
+    return (
+      <React.Fragment>
+            {this.state.acceptedTOC ?
+            null:
+            <div className="footer">
+              <p>By continuing to browse the site you agree to our storing of cookies on your browser to enhance site navigation as well agree to our <a href="https://public.singularitynet.io/Beta-TOS.html" target="_blank">Terms and conditions</a></p>
+              <button className="btn btn-primary" onClick={this.handleShowMessage}>Accept</button>
+            </div>}
+      </React.Fragment>
+    );
+  }
+}
