857be4671ba40dd3b0fabeef807f37f222f7b898
raamb <raam.bd@gmail.com>
2019-04-25 09:34:50 +0530

    Ensure that the default account in web3 is setup always

--- src/sandbox/JobDetailsStandalone.js
+++ src/sandbox/JobDetailsStandalone.js
@@ -1,6 +1,7 @@
 import { hasOwnDefinedProperty } from '../util'
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import {Jobdetails} from '../components/JobDetails.js';
+import React, { Fragment } from 'react';
 
 export  class JobdetailsStandalone extends Jobdetails {
     constructor(props) {
@@ -32,6 +33,10 @@ export  class JobdetailsStandalone extends Jobdetails {
     }
 
     render() {
-      return super.render();
+      return(
+      <Fragment>
+          {super.render()}
+      </Fragment>
+      )
     }
 }
--- src/sandbox/Standalone.js
+++ src/sandbox/Standalone.js
@@ -15,7 +15,7 @@ class Standalone extends React.Component {
       proto:'syntax = "proto3";  package example_service;  message Numbers {     float a = 1;     float b = 2; }  message Result {     float value = 1; }  service Calculator {     rpc add(Numbers) returns (Result) {}     rpc sub(Numbers) returns (Result) {}     rpc mul(Numbers) returns (Result) {}     rpc div(Numbers) returns (Result) {} }',
       endpoint:'http://localhost:7052',
       errorMessage:'',
-      runJob: true,
+      userAddress:undefined
     };
 
     this.network = new BlockchainHelper();
@@ -23,16 +23,29 @@ class Standalone extends React.Component {
     this.serviceMappings = new ServiceMappings();
 
     this.handleField = this.handleField.bind(this);
-    this.onOpenJobDetailsSlider = this.onOpenJobDetailsSlider.bind(this)
+    this.onOpenJobDetailsSlider = this.onOpenJobDetailsSlider.bind(this);
+    this.fetchWeb3Account = this.fetchWeb3Account.bind(this);
   }
 
   componentDidMount() {
     window.addEventListener('load', () => this.handleWindowLoad());
     this.handleWindowLoad();
+    this.fetchWeb3Account();
   }
 
   handleWindowLoad() {
     this.network.initialize();
+  }
+
+  handleField(e) {
+    this.setState({errorMessage: ''})
+    const { name, value } = e.target;
+    this.setState({[name]: value,})
+  }
+
+  runJob() {
+    this.errorMessage = ''
+    this.generate_service_spec_json(this.proto);
   }
 
   generate_service_spec_json() {
@@ -45,23 +58,11 @@ class Standalone extends React.Component {
         this.onOpenJobDetailsSlider();
     }
     catch(ex) {
-        this.setState({runJob: false})
         this.setState({errorMessage: "Exception while parsing proto [" + ex + "]"});
         console.log("Unable to generate service spec json. Please ensure that " + this.proto + " is a valid proto file")
         console.log(ex)
     }
 }
-
-  handleField(e) {
-    this.setState({runJob: true})
-    const { name, value } = e.target;
-    this.setState({[name]: value,})
-  }
-
-  runJob() {
-    this.errorMessage = ''
-    this.generate_service_spec_json(this.proto);
-  }
 
   onOpenJobDetailsSlider() {
     let serviceState = {}
@@ -75,11 +76,17 @@ class Standalone extends React.Component {
     this.refs.jobdetailsComp.onOpenJobDetails(serviceState);
   }
 
+  async fetchWeb3Account(){
+    const getAccountDetails = await web3.eth.getAccounts;
+    getAccountDetails((e,accounts)=>{
+      this.setState({userAddress:accounts[0]});
+    })
+  }
+  
   renderBase() {
     return(
       <React.Fragment>
         <div className="standalone-container">
-
           <div className="row">
             <div className="col-xs-5 col-sm-4 col-md-5 mtb-10">
               Org ID (Used to look up servicemappings)
@@ -119,21 +126,19 @@ class Standalone extends React.Component {
           <div className="col-xs-12 col-sm-12 col-md-12 text-center">
             <button type="button" className="btn btn-primary width-mobile-100" onClick={()=>this.runJob()}>Execute Component</button>
           </div>
-          {!this.state.runJob ?
+          {(this.state.errorMessage === '') ?
+            null :
             <div className="col-xs-5 col-sm-4 col-md-5 mtb-10 error-msg">
                 {this.state.errorMessage}
-            </div>
-            : null
+            </div>            
           }
         </div>
-
-        {this.state.runJob ?
+        
         <JobdetailsStandalone ref="jobdetailsComp" 
-          userAddress= {web3.eth.defaultAccount}
+          userAddress= {this.state.userAddress}
           chainId={this.network.chainId}
           network={this.network}
           reloadDetails={()=>{}}/>
-          : null}
       </React.Fragment>
     )
   }
